5274fca4b1882faea7fab2cd37516d7f4692a7a9
Ezekiel <umanmah234@gmail.com>
2023-01-30 18:11:10 -0500

    add inital Note entity

---
+++ starter/src/main/java/com/devmountain/noteApp/Entities/Note.java
@@ -0,0 +1,35 @@
+package com.devmountain.noteApp.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Notes")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "text")
+    private String body;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
