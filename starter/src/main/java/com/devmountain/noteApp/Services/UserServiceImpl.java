a15d7a6aa5c2c892dafe9c86b0285b23e1c8e8ef
Ezekiel <umanmah234@gmail.com>
2023-02-01 06:59:07 -0500

    add User Service

---
+++ starter/src/main/java/com/devmountain/noteApp/Services/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.devmountain.noteApp.Services;
+
+import com.devmountain.noteApp.DTOs.UserDTO;
+import com.devmountain.noteApp.Entities.User;
+import com.devmountain.noteApp.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+}
