0ffd846e4318d1731e1c7a28068be060740f67d7
Ezekiel <umanmah234@gmail.com>
2023-02-01 06:55:55 -0500

    add findByUsername method

--- starter/src/main/java/com/devmountain/noteApp/Repositories/UserRepository.java
+++ starter/src/main/java/com/devmountain/noteApp/Repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package com.devmountain.noteApp.Repositories;
 
 import com.devmountain.noteApp.Entities.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
