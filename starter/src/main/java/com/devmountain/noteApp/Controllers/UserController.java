9bcc2032e9b790e0135ae1924ebdf121012c7549
Ezekiel <umanmah234@gmail.com>
2023-02-02 10:25:32 -0500

    add user controller

---
+++ starter/src/main/java/com/devmountain/noteApp/Controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.devmountain.noteApp.Controllers;
+
+import com.devmountain.noteApp.DTOs.UserDTO;
+import com.devmountain.noteApp.Services.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+    @Autowired
+    UserServiceImpl userService;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public List<String> addUser(UserDTO userDTO){
+        String passHash = passwordEncoder.encode(userDTO.getPassword());
+        userDTO.setPassword(passHash);
+        return userService.addUser(userDTO);
+    }
+
+    @PostMapping("/login")
+    public List<String> userLogin(UserDTO userDTO){
+        return userService.userLogin(userDTO);
+    }
+}
