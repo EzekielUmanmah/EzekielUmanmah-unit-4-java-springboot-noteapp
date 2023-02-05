20dbbe567a5987b316f32c088c54a585b79926e9
Ezekiel <umanmah234@gmail.com>
2023-02-05 13:27:10 -0500

    change import and add correct annotations

--- starter/src/main/java/com/devmountain/noteApp/Controllers/UserController.java
+++ starter/src/main/java/com/devmountain/noteApp/Controllers/UserController.java
@@ -5,6 +5,7 @@ import com.devmountain.noteApp.Services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,14 +20,14 @@ public class UserController {
     PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
-    public List<String> addUser(UserDTO userDTO){
+    public List<String> addUser(@RequestBody UserDTO userDTO){
         String passHash = passwordEncoder.encode(userDTO.getPassword());
         userDTO.setPassword(passHash);
         return userService.addUser(userDTO);
     }
 
     @PostMapping("/login")
-    public List<String> userLogin(UserDTO userDTO){
+    public List<String> userLogin(@RequestBody UserDTO userDTO){
         return userService.userLogin(userDTO);
     }
 }
